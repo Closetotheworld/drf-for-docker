b83549781518863d083449f976d0ef336da3ba08
closetotheworld <remantion@nate.com>
2021-01-29 20:18:12 +0900

    [ADD] gitignore

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Routine(models.Model):
+    title = models.CharField(max_length=50)
+    category = models.CharField(max_length=30)
+    author = models.CharField(max_length=30)
